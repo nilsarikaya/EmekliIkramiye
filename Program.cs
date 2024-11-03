bbb0c8e16abbee036dd45c7aed6a115c002cc11c
ceyli <ceyli@MSI>
2024-11-03 23:11:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,252 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nt sayi = 6;
+
+            //if (sayi == 1)
+            //{
+            // Console.WriteLine("Sayi 1");
+            //}
+            //else if (sayi == 2) { }
+            //else if (sayi == 3) { }
+            //else if (sayi == 4) { }
+            //else if (sayi == 5) { }
+            //else{ }
+
+            //switch (sayi)
+            //{
+            //    case 1: //sayi==1
+            //        Console.WriteLine("Sayı 1");
+            //        break; // kırmak - kontrol durumunu devam ettirmiyor. Switch case yapısından çıkar.
+
+            //    case 2:
+            //        Console.WriteLine("Sayı 2");
+            //        break;
+
+            //    case 3:
+            //        Console.WriteLine("Sayı 3");
+            //        break;
+
+            //    default: //else
+            //        Console.WriteLine("Sayı 1 - 5 aralığında değil");
+            //        break;
+            //}
+
+
+            #region  Klavyeden iki sayı, bir işlem(+,-,*,/) girilecek. Sayılar girilen işleme göre sonucu ekrana yazdıran switch case.
+
+            //Console.Write("Birinci sayıyı girin: ");
+            //double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("İkinci sayıyı girin: ");
+            //double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Bir işlem girin (+, -, *, /): ");
+            //string islemTuru = Console.ReadLine();
+
+            //double sonuc;
+
+            //if (islemTuru == "+")
+            //{
+            //    sonuc = sayi1 + sayi2;
+            //}
+            //else if (islemTuru == "-")
+            //{
+            //    sonuc = sayi1 - sayi2;
+            //}
+            //else
+            //{
+            //    sonuc = 0;
+            //}
+            //Console.WriteLine("Sonuç:"+sonuc);
+
+
+            //switch (islemTuru)
+            //{
+            //    case "+":
+            //        sonuc = sayi1 + sayi2;
+            //        break;
+            //    case "-":
+            //        sonuc = sayi1 - sayi2;
+            //        break;
+            //    case "*":
+            //        sonuc = sayi1 * sayi2;
+            //        break;
+            //    case "/":
+            //        if (sayi2 != 0)
+            //        {
+            //            sonuc = sayi1 / sayi2;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Bir sayıyı sıfıra bölemezsiniz!");
+            //            sonuc = 0;
+            //        }
+            //        break;
+            //    default:
+            //        Console.WriteLine("Geçersiz işlem!");
+            //        sonuc = 0;
+            //        break;
+            //}
+
+            //Console.WriteLine("Sonuç: " + sonuc);
+
+
+
+            #endregion
+
+            #region 
+            //Console.WriteLine("Birinci sayıyı giriniz");
+            //double sayi1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("İkinci sayıyı giriniz");
+            //double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //Console.Write("Yapmak istediğiniz işlemi girin (+, -, *, /): ");
+            //char islem = Convert.ToChar(Console.ReadLine());
+
+            //double sonuc;
+
+            //switch (islem)
+            //{
+            //    case '+':
+            //        sonuc = sayi1 + sayi2;
+            //        Console.WriteLine("Sonuç: " + sonuc);
+            //        break;
+            //    case '-':
+            //        sonuc = sayi1 - sayi2;
+            //        Console.WriteLine("Sonuç : " + sonuc);
+
+            //        break;
+
+            //    case '*':
+            //        sonuc = sayi1 * sayi2;
+            //        Console.WriteLine("Sonuç : " + sonuc);
+            //        break;
+
+            //    case '/':
+
+            //        if (sayi2 != 0) // Bölme işleminde payda sıfır olmamalı
+            //        {
+            //            sonuc = sayi1 / sayi2;
+            //            Console.WriteLine("Sonuç: " + sonuc);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Hata: Sıfıra bölme yapılamaz!");
+            //        }
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Geçersiz işlem seçildi.");
+            //        break;
+
+            //}
+            #endregion
+
+            /*
+          kullanıcıdan alınan cinsiyet bilgisine göre
+          ==> ERKEK ise
+          yaşı 60 ve üstü ise maaşının 10 katı kadar ikramiye alaral emekli edilecek, yaş 60'ın altında ise çalıştığı gün sayısına göre eğer 6000 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek,    6000 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
+          ==> KADIN ise
+           yaşı 58 ve üstü ise maaşının 10 katı kadar ikramiye alarak emekli edilecek, yaş 58'ın altında ise çalıştığı gün sayısına göre eğer 3600 ve üstü ise maaşının 11 katı kadar ikramiye alarak emekli edilecek, 3600 altında ise emekli edilmeyecek bilgisi kullanıcıya gösterilecek
+          ==> cinsiyet bilgisi switch-case ile sorgulanacak
+           */
+
+
+            #region NİL SARIKAYA
+
+
+
+            Console.WriteLine("Lütfen cinsiyet (KADIN/ERKEK) giriniz:  ");
+            string cinsiyet = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Lütfen yaşınızı giriniz:   ");
+            int yas = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen çalıştığınız gün sayısını giriniz: ");
+            long gun = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Lütfen maaşınızı giriniz:  ");
+            decimal maas = Convert.ToDecimal(Console.ReadLine());
+
+            switch (cinsiyet)
+
+            {
+                case "ERKEK":
+
+                    if (yas >= 60)
+                    {
+                        decimal ikramiye = maas * 10;
+                        Console.WriteLine($"Emekli edilecek. İkramiye: {ikramiye}");
+
+                    }
+
+                    else if (yas < 60)
+                    {
+                        if (gun >= 6000)
+                        {
+                            decimal ikramiye = maas * 11;
+                            Console.WriteLine($"Emekli edilecek. İkramiye: {ikramiye}");
+
+                        }
+
+                        else if (gun < 6000)
+                        {
+                            Console.WriteLine($"Emekli edilemeyecek");
+
+
+                        }
+
+                    }
+
+                    break;
+
+
+                case "KADIN":
+
+                    if (yas >= 58)
+
+                    {
+                        decimal ikramiye = maas * 10;
+                        Console.WriteLine($"Emekli edilecek. İkramiye {ikramiye}");
+
+                    }
+
+                    else if (yas <58)
+                    {
+                        if (gun >= 3600)
+                        {
+                            decimal ikramiye = maas * 11;
+                            Console.WriteLine($"Emekli edilecek. İkramiye {ikramiye}");
+
+
+                        }
+
+                        else if (gun <3600)
+                        {
+
+                            Console.WriteLine("Emekli edilmeyecek.");
+
+                        }
+
+                    }
+
+                    break;
+
+                    default:
+
+                    Console.WriteLine("Geçersiz cinsiyet girdiniz. Tekrar deneyiniz!");
+                    break;
+
+
+                    #endregion
+
+            }
+        }
+    }
+}
+    
